095fbe17c099749d6cd74931b6b13a74f3d41173
Aaron Priven <aaron@priven.com>
2017-06-05 11:41:26 -0700

    Removed "BEGIN" and made doc fixes
    Removed the "BEGIN" block since there's no practical difference
    -- the code will be invoked as soon as the code is finished compiling
    anyway.
    
    Documentation additions (notably "bugs and limitations").

--- lib/English/Control.pm
+++ lib/English/Control.pm
@@ -3,92 +3,89 @@ use 5.008001;
 use strict;
 use warnings;
 
-our $VERSION = "0.01";
+our $VERSION = "0.001_001";
 
-BEGIN {
+*{^ARG} = *_;
 
-    *{^ARG} = *_;
+# Matching.
 
-    # Matching.
+*{^LAST_PAREN_MATCH}     = *+;
+*{^LAST_SUBMATCH_RESULT} = *^N;
+*{^LAST_MATCH_START}     = *-{ARRAY};
+*{^LAST_MATCH_END}       = *+{ARRAY};
 
-    *{^LAST_PAREN_MATCH}     = *+;
-    *{^LAST_SUBMATCH_RESULT} = *^N;
-    *{^LAST_MATCH_START}     = *-{ARRAY};
-    *{^LAST_MATCH_END}       = *+{ARRAY};
+# Input.
 
-    # Input.
+*{^INPUT_LINE_NUMBER}      = *.;
+*{^NR}                     = *.;
+*{^INPUT_RECORD_SEPARATOR} = */;
+*{^RS}                     = */;
 
-    *{^INPUT_LINE_NUMBER}      = *.;
-    *{^NR}                     = *.;
-    *{^INPUT_RECORD_SEPARATOR} = */;
-    *{^RS}                     = */;
+# Output.
 
-    # Output.
+*{^OUTPUT_AUTOFLUSH}        = *|;
+*{^OUTPUT_FIELD_SEPARATOR}  = *,;
+*{^OFS}                     = *,;
+*{^OUTPUT_RECORD_SEPARATOR} = *\;
+*{^ORS}                     = *\;
 
-    *{^OUTPUT_AUTOFLUSH}        = *|;
-    *{^OUTPUT_FIELD_SEPARATOR}  = *,;
-    *{^OFS}                     = *,;
-    *{^OUTPUT_RECORD_SEPARATOR} = *\;
-    *{^ORS}                     = *\;
+# Interpolation "constants".
 
-    # Interpolation "constants".
+*{^LIST_SEPARATOR} = *";
+#   "    # the extra quote restores syntax checking to sanity in vim
+*{^SUBSCRIPT_SEPARATOR} = *;;
+*{^SUBSEP}              = *;;
 
-    *{^LIST_SEPARATOR}      = *";
-    *{^SUBSCRIPT_SEPARATOR} = *;;
-    *{^SUBSEP}              = *;;
+# Formats
 
-    # Formats
+*{^FORMAT_PAGE_NUMBER}           = *%;
+*{^FORMAT_LINES_PER_PAGE}        = *=;
+*{^FORMAT_LINES_LEFT}            = *-{SCALAR};
+*{^FORMAT_NAME}                  = *~;
+*{^FORMAT_TOP_NAME}              = *^;
+*{^FORMAT_LINE_BREAK_CHARACTERS} = *:;
+*{^FORMAT_FORMFEED}              = *^L;
 
-    *{^FORMAT_PAGE_NUMBER}           = *%;
-    *{^FORMAT_LINES_PER_PAGE}        = *=;
-    *{^FORMAT_LINES_LEFT}            = *-{SCALAR};
-    *{^FORMAT_NAME}                  = *~;
-    *{^FORMAT_TOP_NAME}              = *^;
-    *{^FORMAT_LINE_BREAK_CHARACTERS} = *:;
-    *{^FORMAT_FORMFEED}              = *^L;
+# Error status.
 
-    # Error status.
+*{^CHILD_ERROR}       = *?;
+*{^OS_ERROR}          = *!;
+*{^ERRNO}             = *!;
+*{^OS_ERROR}          = *!;
+*{^ERRNO}             = *!;
+*{^EXTENDED_OS_ERROR} = *^E;
+*{^EVAL_ERROR}        = *@;
 
-    *{^CHILD_ERROR}       = *?;
-    *{^OS_ERROR}          = *!;
-    *{^ERRNO}             = *!;
-    *{^OS_ERROR}          = *!;
-    *{^ERRNO}             = *!;
-    *{^EXTENDED_OS_ERROR} = *^E;
-    *{^EVAL_ERROR}        = *@;
+# Process info.
 
-    # Process info.
+*{^PROCESS_ID}         = *$;
+*{^PID}                = *$;
+*{^REAL_USER_ID}       = *<;
+*{^UID}                = *<;
+*{^EFFECTIVE_USER_ID}  = *>;
+*{^EUID}               = *>;
+*{^REAL_GROUP_ID}      = *(;
+*{^GID}                = *(;
+*{^EFFECTIVE_GROUP_ID} = *);
+*{^EGID}               = *);
+*{^PROGRAM_NAME}       = *0;
 
-    *{^PROCESS_ID}         = *$;
-    *{^PID}                = *$;
-    *{^REAL_USER_ID}       = *<;
-    *{^UID}                = *<;
-    *{^EFFECTIVE_USER_ID}  = *>;
-    *{^EUID}               = *>;
-    *{^REAL_GROUP_ID}      = *(;
-    *{^GID}                = *(;
-    *{^EFFECTIVE_GROUP_ID} = *);
-    *{^EGID}               = *);
-    *{^PROGRAM_NAME}       = *0;
+# Internals.
 
-    # Internals.
-
-    *{^PERL_VERSION}            = *^V;
-    *{^OLD_PERL_VERSION}        = *];
-    *{^ACCUMULATOR}             = *^A;
-    *{^COMPILING}               = *^C;
-    *{^DEBUGGING}               = *^D;
-    *{^SYSTEM_FD_MAX}           = *^F;
-    *{^INPLACE_EDIT}            = *^I;
-    *{^PERLDB}                  = *^P;
-    *{^LAST_REGEXP_CODE_RESULT} = *^R;
-    *{^EXCEPTIONS_BEING_CAUGHT} = *^S;
-    *{^BASETIME}                = *^T;
-    *{^WARNING}                 = *^W;
-    *{^EXECUTABLE_NAME}         = *^X;
-    *{^OSNAME}                  = *^O;
-
-} ## tidy end: BEGIN
+*{^PERL_VERSION}            = *^V;
+*{^OLD_PERL_VERSION}        = *];
+*{^ACCUMULATOR}             = *^A;
+*{^COMPILING}               = *^C;
+*{^DEBUGGING}               = *^D;
+*{^SYSTEM_FD_MAX}           = *^F;
+*{^INPLACE_EDIT}            = *^I;
+*{^PERLDB}                  = *^P;
+*{^LAST_REGEXP_CODE_RESULT} = *^R;
+*{^EXCEPTIONS_BEING_CAUGHT} = *^S;
+*{^BASETIME}                = *^T;
+*{^WARNING}                 = *^W;
+*{^EXECUTABLE_NAME}         = *^X;
+*{^OSNAME}                  = *^O;
 
 1;
 
@@ -100,6 +97,10 @@ __END__
 
 English::Control - use names beginning with control for punctuation variables
 
+=head1 VERSION
+
+This document refers to version 0.001
+
 =head1 SYNOPSIS
 
     use English::Control;
@@ -107,28 +108,26 @@ English::Control - use names beginning with control for punctuation variables
 
 =head1 DESCRIPTION
 
-This module provides aliases for the built-in variables whose
-names no one seems to like to read.  Variables with side-effects
-which get triggered just by accessing them (like $0) will still 
-be affected.
+This module provides aliases for the built-in variables whose names no one
+seems to like to read. Variables with side-effects which get triggered just by
+accessing them (like $0) will still be affected.
 
-Unlike the L<English|English> module, the aliases created by this
-module begin with a control character and thus will be always 
-found in the main namespace and are in the area reserved by perl
-for its own use, and don't clutter your regular namespace.
+Unlike the L<English|English> module, the aliases created by this module begin
+with a control character, and thus will be always found in package "main", in
+the area reserved by perl for its own use. They don't clutter the namespace
+used by your program's variables or by any other modules.
 
-For those variables that have an B<awk> version, both long
-and short English alternatives are provided.  For example, 
-the C<$/> variable can be referred to either $^{RS} or 
-${INPUT_RECORD_SEPARATOR} if you are using this module.
+For those variables that have an B<awk> version, both long and short English
+alternatives are provided. For example,  the C<$/> variable can be referred to
+either ${^RS} or  ${^INPUT_RECORD_SEPARATOR} if you are using this module.
 
-See L<perlvar> for a complete list of these. Except for $MATCH, $PREMATCH,
-and $POSTMATCH, all the multi-character names are used.
+See L<perlvar> for a complete list of these. Except for $MATCH, $PREMATCH, and
+$POSTMATCH, all the multi-character names are used.
 
-Since control-character variables are forced to be in package main, nothing
-is imported into the caller's namespace, and the control-character aliases 
-are made immediately upon compilation of this module. 
-There is no difference between
+Since control-character variables are forced to be in package main, nothing is
+imported into the caller's namespace, and the control-character aliases are
+made immediately upon compilation of this module. There is no difference
+between
 
    use English::Control;
 
@@ -138,9 +137,20 @@ and
 
 =head1 PERFORMANCE
 
-This module does not touch the match variables that slow down older
-versions of perl.  The aliases ${^PREMATCH}, ${MATCH}, and ${^POSTMATCH} 
-are provided by perl itself in more recent versions.
+This module does not touch the match variables that slow down older versions of
+perl. The aliases ${^PREMATCH}, ${MATCH}, and ${^POSTMATCH}  are provided by
+perl itself in more recent versions.
+
+=head1 BUGS AND LIMITATIONS
+
+Unlike regular variables, variables that begin with control characters are
+exempt from the strictures imposed by C<use strict 'vars'>. Mistyped variable
+names (e.g, "${^ERRRNO}" instead of "${ERRNO}") will not be flagged by the
+compiler. Thanks to Aristotle Pagaltzis for pointing this out.
+
+Since these variables are in the space reserved by Perl for its own use,  it
+may be that a future version of Perl will break this module by using some of
+these variable names for a different purpose.
 
 =head1 AUTHOR
 
@@ -148,27 +158,30 @@ Aaron Priven <apriven@actransit.org>
 
 =head1 BASED ON
 
-English::Control is a pretty simple fork of 
-L<English|English> from the Perl 5 distribution.
+English::Control is a pretty simple modification of L<English|English> from
+the Perl 5 distribution, by Larry Wall et al.
 
 =head1 COPYRIGHT & LICENSE
 
 Copyright 2017
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of either:
+This program is free software; you can redistribute it and/or modify it under
+the terms of either:
 
 =over 4
 
-=item * the GNU General Public License as published by the Free
-Software Foundation; either version 1, or (at your option) any
-later version, or
+=item *
 
-=item * the Artistic License version 2.0.
+the GNU General Public License as published by the Free Software Foundation;
+either version 1, or (at your option) any later version, or
+
+=item *
+
+the Artistic License version 2.0.
 
 =back
 
-This program is distributed in the hope that it will be useful, but
-WITHOUT  ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or  FITNESS FOR A PARTICULAR PURPOSE.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.
 
